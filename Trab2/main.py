3d7bd8535777d433015ee904f65818ff5d6efdfb
mayaradvh <69974918+mayaradvh@users.noreply.github.com>
2024-10-29 15:15:57 -0300

    RGB e verificar se esta igual blur do openCV

--- Trab2/main.py
+++ Trab2/main.py
@@ -29,8 +29,8 @@ def ingenuo (img_original):
 				img[l_index][c_index] = 0 
 				continue
 			soma = 0
-			for l in range(l_index - math.floor(JANELA/2), l_index + math.floor(JANELA/2)):
-				for c in range(c_index - math.floor(JANELA/2), c_index + math.floor(JANELA/2)): 
+			for l in range(l_index - math.floor(JANELA/2), l_index + math.floor(JANELA/2)+1):
+				for c in range(c_index - math.floor(JANELA/2), c_index + math.floor(JANELA/2)+1): 
 					soma = soma + img_original[l][c]
 			media = soma / (JANELA*JANELA)	
 			img[l_index][c_index] = media
@@ -42,27 +42,28 @@ def separavel(img_original):
 	for l_index, linha in enumerate(img_original):
 		for c_index, coluna in enumerate(linha):
 			if c_index < math.floor(JANELA/2) or c_index > (len(linha) - math.floor(JANELA/2) - 1):
-				img[l_index][c_index] = 0 
+				#img[l_index][c_index] = 0 
 				continue
 			soma = 0
-			for c in range(c_index - math.floor(JANELA/2), c_index + math.floor(JANELA/2)): 
+			for c in range(c_index - math.floor(JANELA/2), c_index + math.floor(JANELA/2)+1): 
 				soma = soma + img_original[l_index][c]
 			media = soma / JANELA
 			img[l_index][c_index] = media
 			
+	img2 = img.copy()
 	for l_index, linha in enumerate(img):
 		if l_index < math.floor(JANELA/2) or l_index > (len(img) - math.floor(JANELA/2) - 1):
 			continue
 		for c_index, coluna in enumerate(linha):
 			if c_index < math.floor(JANELA/2) or c_index > (len(linha) - math.floor(JANELA/2) - 1):
-				img[l_index][c_index] = 0 
+				#img2[l_index][c_index] = 0 
 				continue
 			soma = 0
-			for l in range(l_index - math.floor(JANELA/2), l_index + math.floor(JANELA/2)): 
+			for l in range(l_index - math.floor(JANELA/2), l_index + math.floor(JANELA/2)+1): 
 				soma = soma + img[l][c_index]
 			media = soma / JANELA
-			img[l_index][c_index] = media			
-	return img
+			img2[l_index][c_index] = media			
+	return img2
 	
 def integral(img_original):
 	img_integral = img_original.copy()
@@ -110,49 +111,69 @@ def integral(img_original):
 				img[l_index][c_index] = media
 				
 	return img
-				
+
+def algoritmo_RGB(func, img_original):
+	img = img_original.copy()
+	for i in range(0, 3):
+		img[:, :, i] =  func(img_original[:, :, i])
+	return img[math.floor(JANELA/2):-math.floor(JANELA/2), math.floor(JANELA/2):-math.floor(JANELA/2), :]
+
+def verifica_igual(img1, img2):
+	flag = False
+	if np.array_equal(img1, img2):
+		flag = True
+	else:
+		for x, _ in enumerate(img1):
+			for y, _ in enumerate(img1[x]):
+				for z, _ in enumerate(img1[x][y]):
+					if img1[x][y][z] == img2[x][y][z] or img1[x][y][z]-1 == img2[x][y][z] or img1[x][y][z]+1 == img2[x][y][z]:
+						flag = True
+					else:
+						flag = False
+						break
+	return flag
+
 #===============================================================================
 
 def main ():
 
     # Abre a imagem em escala de cinza.
-    img = cv2.imread (INPUT_IMAGE, cv2.IMREAD_GRAYSCALE)
-    if img is None:
-        print ('Erro abrindo a imagem.\n')
-        sys.exit ()
+	img = cv2.imread (INPUT_IMAGE, cv2.IMREAD_COLOR)
+	if img is None:
+		print ('Erro abrindo a imagem.\n')
+		sys.exit ()
 
     # É uma boa prática manter o shape com 3 valores, independente da imagem ser
     # colorida ou não. Também já convertemos para float32.
-    img = img.reshape ((img.shape [0], img.shape [1], 1))
-    img = img.astype (np.float32) / 255
+	img = img.astype (np.float32) / 255
+
+	blur = cv2.blur(img,(JANELA,JANELA))
+	blur = blur[math.floor(JANELA/2):-math.floor(JANELA/2), math.floor(JANELA/2):-math.floor(JANELA/2), :]
+	blur = (blur*255).astype(np.uint8)
+	cv2.imwrite ('04 - blur.png', blur)
+
+	img_ingenuo = (algoritmo_RGB(ingenuo, img)*255).astype(np.uint8)
+	img_separavel = (algoritmo_RGB(separavel, img)*255).astype(np.uint8)
+	img_integral = (algoritmo_RGB(integral, img)*255).astype(np.uint8)
     
-    #img_ingenuo = ingenuo(img)
-    #img_separavel = separavel(img)
-    img_integral = integral(img)
+	cv2.imwrite ('01 - ingenuo.png', img_ingenuo)
+	cv2.imwrite ('02 - separavel.png', img_separavel)
+	cv2.imwrite ('03 - integral.png', img_integral)
+      
+	if verifica_igual(blur, img_ingenuo):
+		print("Algoritmo ingênuo corretamente implementado")
+	else:
+		print("Algoritmo ingênuo NÃO corretamente implementado")
+	if verifica_igual(blur, img_separavel):
+		print("Algoritmo separavel corretamente implementado")
+	else:
+		print("Algoritmo separavel NÃO corretamente implementado")
+	if verifica_igual(blur, img_integral):
+		print("Algoritmo integral corretamente implementado")
+	else: 
+		print("Algoritmo integral NÃO corretamente implementado")
 
-    # Segmenta a imagem.
-    #cv2.imshow ('01 - ingenuo', img_ingenuo)
-    
-    #cv2.imwrite ('01 - ingenuo.png', img_ingenuo*255)
-    #cv2.imwrite ('02 - separavel.png', img_separavel*255)
-    cv2.imwrite ('03 - integral.png', img_integral*255)
-        
-    exit()
-
-    start_time = timeit.default_timer ()
-    componentes = rotula (img, LARGURA_MIN, ALTURA_MIN, N_PIXELS_MIN)
-    n_componentes = len (componentes)
-    print ('Tempo: %f' % (timeit.default_timer () - start_time))
-    print ('%d componentes detectados.' % n_componentes)
-
-    # Mostra os objetos encontrados.
-    for c in componentes:
-        cv2.rectangle (img_out, (c ['L'], c ['T']), (c ['R'], c ['B']), (0,0,1))
-
-    cv2.imshow ('02 - out', img_out)
-    cv2.imwrite ('02 - out.png', img_out*255)
-    cv2.waitKey ()
-    cv2.destroyAllWindows ()
+	exit()
 
 
 if __name__ == '__main__':
