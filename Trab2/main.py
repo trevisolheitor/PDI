6e5e32100317d32a0d1a1bd1cf4b7bc5e1fc49d7
heitordt <trevisolheitor@gmail.com>
2024-10-30 14:53:05 -0300

    Minor fixes

--- Trab2/main.py
+++ Trab2/main.py
@@ -16,21 +16,22 @@ import math
 INPUT_IMAGE =  'tom.jpeg'
 
 JANELA = 33
+MARGEM = math.floor(JANELA/2)
 
 #===============================================================================
 
 def ingenuo (img_original):
 	img = img_original.copy()
 	for l_index, linha in enumerate(img_original):
-		if l_index < math.floor(JANELA/2) or l_index > (len(img) - math.floor(JANELA/2) - 1):
+		if l_index < MARGEM or l_index > (len(img) - MARGEM - 1):
 			continue
 		for c_index, coluna in enumerate(linha):
-			if c_index < math.floor(JANELA/2) or c_index > (len(linha) - math.floor(JANELA/2) - 1):
+			if c_index < MARGEM or c_index > (len(linha) - MARGEM - 1):
 				img[l_index][c_index] = 0 
 				continue
 			soma = 0
-			for l in range(l_index - math.floor(JANELA/2), l_index + math.floor(JANELA/2)+1):
-				for c in range(c_index - math.floor(JANELA/2), c_index + math.floor(JANELA/2)+1): 
+			for l in range(l_index - MARGEM, l_index + MARGEM+1):
+				for c in range(c_index - MARGEM, c_index + MARGEM+1): 
 					soma = soma + img_original[l][c]
 			media = soma / (JANELA*JANELA)	
 			img[l_index][c_index] = media
@@ -41,25 +42,25 @@ def separavel(img_original):
 	img = img_original.copy()
 	for l_index, linha in enumerate(img_original):
 		for c_index, coluna in enumerate(linha):
-			if c_index < math.floor(JANELA/2) or c_index > (len(linha) - math.floor(JANELA/2) - 1):
+			if c_index < MARGEM or c_index > (len(linha) - MARGEM - 1):
 				#img[l_index][c_index] = 0 
 				continue
 			soma = 0
-			for c in range(c_index - math.floor(JANELA/2), c_index + math.floor(JANELA/2)+1): 
+			for c in range(c_index - MARGEM, c_index + MARGEM+1): 
 				soma = soma + img_original[l_index][c]
 			media = soma / JANELA
 			img[l_index][c_index] = media
 			
 	img2 = img.copy()
 	for l_index, linha in enumerate(img):
-		if l_index < math.floor(JANELA/2) or l_index > (len(img) - math.floor(JANELA/2) - 1):
+		if l_index < MARGEM or l_index > (len(img) - MARGEM - 1):
 			continue
 		for c_index, coluna in enumerate(linha):
-			if c_index < math.floor(JANELA/2) or c_index > (len(linha) - math.floor(JANELA/2) - 1):
+			if c_index < MARGEM or c_index > (len(linha) - MARGEM - 1):
 				#img2[l_index][c_index] = 0 
 				continue
 			soma = 0
-			for l in range(l_index - math.floor(JANELA/2), l_index + math.floor(JANELA/2)+1): 
+			for l in range(l_index - MARGEM, l_index + MARGEM+1): 
 				soma = soma + img[l][c_index]
 			media = soma / JANELA
 			img2[l_index][c_index] = media			
@@ -84,29 +85,29 @@ def integral(img_original):
 	
 	for l_index, linha in enumerate(img_integral):
 		for c_index, coluna in enumerate(linha):
-				if (((l_index + math.floor(JANELA/2)) > (len(img) - 1)) and ((c_index + math.floor(JANELA/2)) > (len(linha) - 1))):
+				if (((l_index + MARGEM) > (len(img) - 1)) and ((c_index + MARGEM) > (len(linha) - 1))):
 					soma = img_integral[len(img) - 1][len(linha) - 1]
-				elif ((l_index + math.floor(JANELA/2)) > (len(img) - 1)):
-					soma = img_integral[len(img) - 1][c_index + math.floor(JANELA/2)]
-				elif ((c_index + math.floor(JANELA/2)) > (len(linha) - 1)):
-					soma = img_integral[l_index + math.floor(JANELA/2)][len(linha) - 1]
+				elif ((l_index + MARGEM) > (len(img) - 1)):
+					soma = img_integral[len(img) - 1][c_index + MARGEM]
+				elif ((c_index + MARGEM) > (len(linha) - 1)):
+					soma = img_integral[l_index + MARGEM][len(linha) - 1]
 				else:
-					soma = img_integral[l_index + math.floor(JANELA/2)][c_index + math.floor(JANELA/2)]
+					soma = img_integral[l_index + MARGEM][c_index + MARGEM]
 				flag = 0
-				if c_index - math.floor(JANELA/2) > 0:
-					if ((l_index + math.floor(JANELA/2)) > (len(img) - 1)):
-						soma = soma - img_integral[len(img) - 1][c_index - math.floor(JANELA/2) - 1]
+				if c_index - MARGEM > 0:
+					if ((l_index + MARGEM) > (len(img) - 1)):
+						soma = soma - img_integral[len(img) - 1][c_index - MARGEM - 1]
 					else:
-						soma = soma - img_integral[l_index + math.floor(JANELA/2)][c_index - math.floor(JANELA/2) - 1]
+						soma = soma - img_integral[l_index + MARGEM][c_index - MARGEM - 1]
 					flag = flag + 1
-				if l_index - math.floor(JANELA/2) > 0:
-					if ((c_index + math.floor(JANELA/2)) > (len(linha) - 1)):
-						soma = soma - img_integral[l_index - math.floor(JANELA/2) - 1][len(linha) - 1]
+				if l_index - MARGEM > 0:
+					if ((c_index + MARGEM) > (len(linha) - 1)):
+						soma = soma - img_integral[l_index - MARGEM - 1][len(linha) - 1]
 					else:
-						soma = soma - img_integral[l_index - math.floor(JANELA/2) - 1][c_index + math.floor(JANELA/2)]
+						soma = soma - img_integral[l_index - MARGEM - 1][c_index + MARGEM]
 					flag = flag + 1			
 				if flag == 2:
-					soma = soma + img_integral[l_index - math.floor(JANELA/2) - 1][c_index - math.floor(JANELA/2) - 1]
+					soma = soma + img_integral[l_index - MARGEM - 1][c_index - MARGEM - 1]
 				media = soma / (JANELA*JANELA)
 				img[l_index][c_index] = media
 				
@@ -117,7 +118,7 @@ def algoritmo_RGB(func, img_original):
 	for i in range(0, 3):
 		img[:, :, i] =  func(img_original[:, :, i])
 	img = (img*255).astype(np.uint8)
-	return img, img[math.floor(JANELA/2):-math.floor(JANELA/2), math.floor(JANELA/2):-math.floor(JANELA/2), :]
+	return img, img[MARGEM:-MARGEM, MARGEM:-MARGEM, :]
 
 def verifica_igual(img1, img2):
 	flag = False
@@ -146,10 +147,10 @@ def main ():
 
     # É uma boa prática manter o shape com 3 valores, independente da imagem ser
     # colorida ou não. Também já convertemos para float32.
-	img = img.astype (np.float32) / 255
+	img = img.astype (np.float32) / 255.0
 
 	blur = cv2.blur(img,(JANELA,JANELA))
-	blur_sem_margem = blur[math.floor(JANELA/2):-math.floor(JANELA/2), math.floor(JANELA/2):-math.floor(JANELA/2), :]
+	blur_sem_margem = blur[MARGEM:-MARGEM, MARGEM:-MARGEM, :]
 	blur = (blur*255).astype(np.uint8)
 	blur_sem_margem = (blur_sem_margem*255).astype(np.uint8)
 	cv2.imwrite ('04 - blur.png', blur)
