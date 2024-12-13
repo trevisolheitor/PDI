1d5ffab416b55df88cd71ade73874757efcf1cdf
heitordt <trevisolheitor@gmail.com>
2024-12-13 16:11:21 -0300

    Nova funcao green_index

--- Trab5/main.py
+++ Trab5/main.py
@@ -1,3 +1,11 @@
+#===============================================================================
+# Exemplo: Chroma Key
+#-------------------------------------------------------------------------------
+# Autor: Bogdan T. Nassu
+# Aluno: Heitor Derder Trevisol e Mayara Dal Vesco Hoger
+# Universidade Tecnológica Federal do Paraná
+#===============================================================================
+
 import sys
 import numpy as np
 import cv2
@@ -24,7 +32,7 @@ def open_image(img_name):
     return img#.astype (np.float32) / 255.0
 
 def so_fundo(img):
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HLS) 
     back = cv2.imread("fundo.png", cv2.IMREAD_COLOR)
     back = cv2.resize(back, (img.shape[1], img.shape[0]))
     mask = cv2.inRange(hsv, (36, 70, 70), (70, 255,255))
@@ -41,36 +49,95 @@ def so_fundo(img):
 
       
 
-def gradinete(img):
+def gradiente(img):
     imagegray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     lap = cv2.Laplacian(imagegray, cv2.CV_64F)
     lap = np.uint8(np.absolute(lap))
     return lap
-    cv2.imshow("Laplacian", lap)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    #cv2.imshow("Laplacian", lap)
+    #cv2.waitKey()
+    #cv2.destroyAllWindows()
 
 
 def mistura(img, back, grad):
-    img_out = img.copy()
-    grad = grad/255.0
+    img_out = img.copy()    
+#    grad = grad/255.0
 
+#    img[:,:,1] = np.where(grad==0, img[:,:,1], 0)
+    cv2.imshow("add", img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
     for iy, y in enumerate(img):
         for ix, x in enumerate(y):
-            for ic, c in enumerate(x):
-            
+            for ic, c in enumerate(x):            
                 img_out[iy][ix][ic] = (img[iy][ix][ic]*(1-grad[iy][ix])) + back[iy][ix][ic]*(grad[iy][ix])
     
-    return img_out
 #    dst = cv2.addWeighted(img, (1.0-grad), back, grad, 0.0)
     cv2.imshow("add", img_out)
     cv2.waitKey()
     cv2.destroyAllWindows()
+    return img_out
 
-
-
+def green_index_mask(img):
+    hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)    
+            
+    H, L, S = cv2.split(hls)
+    mask = np.zeros_like(H, dtype=np.float32)
+    mask_h = np.zeros_like(H, dtype=np.float32)
+    mask_l = np.zeros_like(L, dtype=np.float32)
+    mask_s = np.zeros_like(S, dtype=np.float32)
+#===============================================================================            
+    peak_green = 60
+    peak_lower_bound = peak_green - 10
+    peak_upper_bound = peak_green + 10
+    ramp_lower_bound = peak_green - 20
+    ramp_upper_bound = peak_green + 20   
+    
+    H_ramp_up = (H >= ramp_lower_bound) & (H < peak_lower_bound)
+    mask_h[H_ramp_up] = (H[H_ramp_up] - ramp_lower_bound)/(peak_lower_bound - ramp_lower_bound)
+    
+    H_peak = (H >= peak_lower_bound) & (H <= peak_upper_bound)
+    mask_h[H_peak] = 1.0
+    
+    H_ramp_down = (H > peak_upper_bound) & (H <= ramp_upper_bound)
+    mask_h[H_ramp_down] = (ramp_upper_bound - H[H_ramp_down])/(ramp_upper_bound - peak_upper_bound)      
+#===============================================================================    
+    sat_threshold = 50
+       
+    sat_ramp = (S >= (sat_threshold-10)) & (S < sat_threshold)
+    mask_s[sat_ramp] = (S[sat_ramp] - (sat_threshold-10))/(sat_threshold - (sat_threshold-10))
+    
+    sat_peak = (S >= sat_threshold)
+    mask_s[sat_peak] = 1.0
+#===============================================================================    
+    lum_peak_lower_bound = 100
+    lum_peak_upper_bound = 180
+    lum_ramp_lower_bound = 50
+    lum_ramp_upper_bound = 230
+    
+    
+    L_ramp_up = (L >= lum_ramp_lower_bound) & (L < lum_peak_lower_bound)
+    mask_l[L_ramp_up] = (L[L_ramp_up] - lum_ramp_lower_bound)/(lum_peak_lower_bound - lum_ramp_lower_bound)
+    
+    L_peak = (L >= lum_peak_lower_bound) & (L <= lum_peak_upper_bound)
+    mask_l[L_peak] = 1.0
+    
+    L_ramp_down = (L > lum_peak_upper_bound) & (L <= lum_ramp_upper_bound)
+    mask_l[L_ramp_down] = (lum_ramp_upper_bound - L[L_ramp_down])/(lum_ramp_upper_bound - lum_peak_upper_bound)    
+#===============================================================================    
+    mask = mask_h*mask_l*mask_s
+    #mask = 1 - mask
+    
+    #mask = cv2.resize(mask, (int(mask.shape[1]*0.7), int(mask.shape[0]*0.7))) 
+    cv2.imshow("green_index", mask)
+    cv2.waitKey()
+    cv2.destroyAllWindows()   
+    
+    return mask         	
+    
+    
 def greenes(img): #calcula a verdicidade de cada pixel
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
 
     back = cv2.imread("fundo.png", cv2.IMREAD_COLOR)
     back = cv2.resize(back, (img.shape[1], img.shape[0]))
@@ -88,7 +155,7 @@ def greenes(img): #calcula a verdicidade de cada pixel
     return res
 
 def verdisse(img):
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
     h, s, v = cv2.split(hsv)
     h = h.astype(float)
     for y, valueY in enumerate(h):
@@ -101,22 +168,24 @@ def verdisse(img):
                        [1, 1, 1], 
                        [0, 1, 0]], dtype=np.uint8)
     h = cv2.dilate(h, kernel, iterations=1) # para tentar tirar ruido
-    cv2.imshow("verdisse", h)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    #cv2.imshow("verdisse", h)
+    #cv2.waitKey()
+    #cv2.destroyAllWindows()
 
 
 def main():
     images = open_all_images(INPUT_FOLDER)
     for img, filename in images:
-        expected = filename.split('.')[0]
-        verdisse(img)
-        # simples = greenes(img)
+        index = green_index_mask(img)
+        #expected = filename.split('.')[0]
+        #verdisse(img)
+        #simples = greenes(img)
         # cv2.imwrite("simples/simples"+filename, simples)
-        # fundo_verde, back = so_fundo(img)
-        # grad = gradinete(fundo_verde)
+        fundo_verde, back = so_fundo(img)
+        #grad = gradiente(fundo_verde)
         # cv2.imwrite("gradiente/gradiente"+filename, grad)
-        # mist = mistura(simples, back, grad)
+        #mist = mistura(simples, back, grad)
+        mist = mistura(img, back, index)
         # cv2.imwrite("mistura/mistura"+filename, mist)
     
     
